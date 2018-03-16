4a59a2e4e2fdfddd4476bce82e777e702845e567
Mikee Rmz <mikee.rmz@gmail.com>
2018-03-16 17:03:45 -0600

    Now only show the id and name of quote in the request
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- app/controllers/api/v1/quote_controller.rb
+++ app/controllers/api/v1/quote_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::QuoteController < ApplicationController
 	def all_quote
 	  quote = Quote.all
 
-	  render json: quote.as_json()
+	  render json: quote, :except => [:created_at, :updated_at]
 	end
 end
