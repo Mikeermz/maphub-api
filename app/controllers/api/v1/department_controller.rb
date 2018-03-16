e9a892ed20c9ae65bc3b35bfa78a6c4ef512152a
Mikee Rmz <mikee.rmz@gmail.com>
2018-03-16 17:03:21 -0600

    Add method post to create survey
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- app/controllers/api/v1/department_controller.rb
+++ app/controllers/api/v1/department_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::DepartmentController < ApplicationController
 	def all_departments
 	  department = Department.all
 
-	  render json: department.as_json()
+	  render json: department, :except => [:created_at, :updated_at]
 	end
+
 	def create_survey
 		email = SurveyDepartment.find_by(email: params[:survey][:email])
 
@@ -23,5 +24,9 @@ class Api::V1::DepartmentController < ApplicationController
 	  		:include => [{:department => {:only => :department_name}}, {:quote => {:only => :qoute_name}}],
 	  		:except => [:created_at, :updated_at, :quote_id, :department_id]
 	  end
+	end
+
+	def survey_general_to_departments
+
 	end
 end
